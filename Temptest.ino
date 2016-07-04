8de6c2a3ffaba2bc1bdc9520869919948a9ee44b
Kevin McAleer <kevin.mcaleer@advicefactory.co.uk>
2016-07-04 21:52:29 +0100

    SPI Added
    Also added LED Blink code on Pin 13.

--- Temptest.ino
+++ Temptest.ino
@@ -9,17 +9,23 @@
 // 
 //
 
+// include the SPI Library:
+#include "SPI.h"
+
 int lightSensorPin = A0;
 int tempSensorPin = A1; // select the input pin for the thermometer
 int temperature = 0;
 float temperatureC = 0.0;
 int light = 0;
+int ledPin = 13;
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600); 
  // If you want to set the aref to something other than 5v
   analogReference(EXTERNAL);
+  SPI.begin();
+  pinMode(ledPin,OUTPUT);
 }
 
 void loop() {
@@ -31,7 +37,10 @@ light = analogRead(lightSensorPin);
 float voltage = temperature * 5.0; // use aref_voltage if 3.3 volts
 voltage /=1024.0;
 temperatureC = (voltage - 0.5) * 100;
+digitalWrite(ledPin, LOW);
  delay(1000);
+ digitalWrite(ledPin, HIGH);
+ 
  Serial.print("temp: ");
  Serial.print(temperature);
  Serial.print(", temperature Degrees C: ");
