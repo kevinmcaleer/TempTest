8853ce40586833f69a05356e7cb61b4d65fae50a
Kevin McAleer <kevin.mcaleer@advicefactory.co.uk>
2016-07-04 21:58:58 +0100

    commented out all other code

--- Temptest.ino
+++ Temptest.ino
@@ -21,10 +21,11 @@ int ledPin = 8;
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600); 
- // If you want to set the aref to something other than 5v
-  analogReference(EXTERNAL);
-  SPI.begin();
+  
+  //Serial.begin(9600); 
+  // If you want to set the aref to something other than 5v
+  //analogReference(EXTERNAL);
+  //SPI.begin();
   pinMode(ledPin,OUTPUT);
 }
 
@@ -32,20 +33,21 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // read the value from the sensor
-  temperature = analogRead(tempSensorPin);
-  light = analogRead(lightSensorPin);
-  float voltage = temperature * 5.0; // use aref_voltage if 3.3 volts
-  voltage /=1024.0;
-  temperatureC = (voltage - 0.5) * 100;
+  
+  //temperature = analogRead(tempSensorPin);
+  //light = analogRead(lightSensorPin);
+  //float voltage = temperature * 5.0; // use aref_voltage if 3.3 volts
+  //voltage /=1024.0;
+  //temperatureC = (voltage - 0.5) * 100;
   digitalWrite(ledPin, LOW);
   delay(1000);
   digitalWrite(ledPin, HIGH);
   delay(1000);
  
-  Serial.print("temp: ");
-  Serial.print(temperature);
-  Serial.print(", temperature Degrees C: ");
-  Serial.print(temperatureC);
-  Serial.print(", light level: ");
-  Serial.println(light);
+  //Serial.print("temp: ");
+  //Serial.print(temperature);
+  //Serial.print(", temperature Degrees C: ");
+  //Serial.print(temperatureC);
+  //Serial.print(", light level: ");
+  //Serial.println(light);
 }
