df98645f82f790f618e084b4e2e08ee130f22b07
Kevin McAleer <kevin.mcaleer@advicefactory.co.uk>
2016-07-08 20:38:00 +0100

    Added commands

--- Temptest.ino
+++ Temptest.ino
@@ -8,10 +8,21 @@
 #define aref_voltage 3.3
 // set above to be 5.0 if 5 volts
 // 
-// Need to add code to write out lines via SPI port
+// Need to add code to write out lines via UART port Digital Pins 0 and 1 (RX, TX)
+//
+// Greenhouse robot takes commands from UART serial
+// then replies back with results of command.
+// Commands:
+//    Temp
+//    Light
+//    Door
+//    Window
+//    version
+//    
 
 // include the SPI Library:
 #include "SPI.h"
+
 
 int lightSensorPin = A0;
 int tempSensorPin = A1; // select the input pin for the thermometer
@@ -19,15 +30,36 @@ int temperature = 0;
 float temperatureC = 0.0;
 int light = 0;
 int ledPin = 13;
+string command;
 
 void setup() {
   // put your setup code here, to run once:
   
-  //Serial.begin(9600); 
+  Serial.begin(9600); 
   // If you want to set the aref to something other than 5v
   //analogReference(EXTERNAL);
   SPI.begin();
   pinMode(ledPin,OUTPUT);
+}
+
+void readCommand()
+{
+  Serial.readln(command);  
+}
+
+void temp()
+{
+  Serial.print("temperature: ");
+  Serial.println(temperatureC);  
+
+  //SPI.write(", light level: ");
+  //SPI.write(light);
+  //Serial.print("temp: ");
+  //Serial.print(temperature);
+  //Serial.print(", temperature Degrees C: ");
+  //Serial.print(temperatureC);
+  //Serial.print(", light level: ");
+  //Serial.println(light);
 }
 
 void loop() {
@@ -45,14 +77,22 @@ void loop() {
   digitalWrite(ledPin, LOW);
   delay(1000);
 
-  SPI.write("temp: ");
-  SPI.write(temperature);
-  SPI.write(", light level: ");
-  SPI.write(light);
-  //Serial.print("temp: ");
-  //Serial.print(temperature);
-  //Serial.print(", temperature Degrees C: ");
-  //Serial.print(temperatureC);
-  //Serial.print(", light level: ");
-  //Serial.println(light);
+  // loop read command
+  readCommand();
+  switch (command) {
+    case "temp":
+    // do something with temperature
+    break;
+    case "light":
+    // do something with light
+    break;
+    default:
+    // do something if command not recognised
+    Serial.writeln("command not recognised");
+    break;
+    }
+  
+  //SPI.write("temp: ");
+  //SPI.write(temperature);
+  
 }
