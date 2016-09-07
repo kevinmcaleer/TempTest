7b7bf3a89294cb6b091275d57c333efd82d860a4
Kevin McAleer <kevin.mcaleer@advicefactory.co.uk>
2016-09-07 23:30:36 +0100

    fixed bug in install tools script

--- install_avr_tools.sh
+++ install_avr_tools.sh
@@ -3,7 +3,7 @@ apt-get install bison flex -y
 wget http://download.savannah.gnu.org/release/avrdude/avrdude-6.2.tar.gz
 tar xfv avrdude-6.2.tar.gz
 cd avrdude-6.2/
-./configure - -enable-linuxgpio
+./configure --enable-linuxgpio
 make
 make install
 sudo nano /usr/local/etc/avrdude.conf
